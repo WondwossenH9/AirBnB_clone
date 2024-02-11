e76597143e53968e686931739a0d02736b7db681
WondwossenH9 <wondwossenhailu7@gmail.com>
2024-02-11 13:24:37 -0800

    Modify the FileStorage class to serialize model instances to a JSON file and deserialize them back into Python objects

--- models/base_model.py
+++ models/base_model.py
@@ -1,62 +1,47 @@
 #!/usr/bin/python3
 """
-This module defines the BaseModel class,
-which serves as the base class for all
-other model classes, providing
-common attributes/methods.
+Defines the BaseModel class.
 """
 
 from uuid import uuid4
 from datetime import datetime
 from models import storage
 
+
 class BaseModel:
-    """
-    BaseModel defines common attributes and methods for other classes.
-    Attributes are set dynamically via the __init__ method,
-    allowing for instantiation from a dictionary representation.
-    """
+    """Defines all common attributes/methods
+    for other classes."""
 
     def __init__(self, *args, **kwargs):
-        """
-        Initializes a new instance. If creating from a dictionary (kwargs),
-        set attributes accordingly. Otherwise, set default attributes and
-        add instance to storage.
-        """
+        """Initializes a new BaseModel instance."""
         if kwargs:
-            # Existing initialization from kwargs...
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                elif key in ('created_at', 'updated_at'):
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                setattr(self, key, value)
         else:
             self.id = str(uuid4())
             self.created_at = self.updated_at = datetime.now()
-            storage.new(self)  # Add the new instance to storage
-
-    def __str__(self):
-        """
-        Returns a string representation of the BaseModel instance.
-        """
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+            storage.new(self)
 
     def save(self):
-        """
-        Calls save() method of storage, updating the updated_at attribute.
-        """
+        """Updates updated_at with current datetime
+        and saves to file."""
         self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
-        """
-        Returns a dictionary containing all keys/values of
-        the instance's __dict__, including the class name
-        under '__class__' and converting datetime attributes
-        to strings in ISO format.
-        """
-        dict_rep = self.__dict__.copy()
+        """Returns a dictionary containing all
+        keys/values of __dict__."""
+        dict_rep = dict(self.__dict__)
         dict_rep['__class__'] = self.__class__.__name__
         dict_rep['created_at'] = self.created_at.isoformat()
         dict_rep['updated_at'] = self.updated_at.isoformat()
-        return dict_repdef save(self):
-        """
-        Calls save() method of storage, updating the updated_at attribute.
-        """
-        self.updated_at = datetime.now()
-        storage.save()
+        return dict_rep
+
+    def __str__(self):
+        """Returns the string representation
+        of the BaseModel instance."""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """
-Defines the FileStorage class that serializes the
-instances to a JSON file and
-deserializes JSON file to instances.
+Defines FileStorage class to serialize and
+deserialize instances to a JSON file.
 """
 
 import json
@@ -10,50 +9,40 @@ from models.base_model import BaseModel
 
 
 class FileStorage:
-    """
-    Serializes instances to a JSON file
-    and deserializes the JSON file
-    to instances.
-    """
-
+    """Serializes instances to a JSON file &
+    deserializes back to instances."""
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """
-        Returns the dictionary __objects.
-        """
+        """Returns the dictionary __objects."""
         return FileStorage.__objects
 
     def new(self, obj):
-        """
-        Sets in __objects the obj with
-        key <obj class name>.id.
-        """
-        key = f"{obj.__class__.__name__}.{obj.id}"
-        FileStorage.__objects[key] = obj
+        """Sets in __objects the obj with
+        key <obj class name>.id."""
+        if obj:
+            key = f"{obj.__class__.__name__}.{obj.id}"
+            self.__objects[key] = obj
 
     def save(self):
-        """
-        Serializes __objects to the JSON
-        file (path: __file_path).
-        """
-        with open(FileStorage.__file_path, 'w') as f:
-            json.dump({k: v.to_dict() for k, v in FileStorage.__objects.items()}, f)
+        """Serializes __objects to the
+        JSON file (path: __file_path)."""
+        obj_dict = {obj: self.__objects[obj].to_dict()
+                    for obj in self.__objects.keys()}
+        with open(self.__file_path, 'w') as f:
+            json.dump(obj_dict, f)
 
     def reload(self):
-        """
-        Deserializes the JSON file to __objects and
-        (if file exists, otherwise do nothing).
-        """
+        """Deserializes the JSON file to
+        __objects, if file exists."""
         try:
-            with open(FileStorage.__file_path, 'r') as f:
-                objects = json.load(f)
-            for key, value in objects.items():
-                cls_name = key.split(".")[0]
-                if cls_name == "BaseModel":
-                    obj = BaseModel(**value)
-                # Extend with other classes as needed
-                FileStorage.__objects[key] = obj
+            with open(self.__file_path, 'r') as f:
+                objs = json.load(f)
+                for obj in objs.values():
+                    cls_name = obj['__class__']
+                    if cls_name == 'BaseModel':
+                        self.__objects[obj['id']] = BaseModel(**obj)
+                    # Include similar conditions for other classes
         except FileNotFoundError:
             pass
