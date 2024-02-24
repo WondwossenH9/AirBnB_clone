40e7f87411766695ac81ebe4c18e8822d883934d
WondwossenH9 <wondwossenhailu7@gmail.com>
2024-02-23 22:44:34 -0800

    Updated base model to re-create an instance with this dictionary representation

--- models/base_model.py
+++ models/base_model.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """
-This module defines the BaseModel class which serves as the base class
-for all other classes including common attributes
-and methods that will be inherited by other classes.
+Updated module to define the BaseModel class to
+support creation from dictionary representation.
 """
 
 import uuid
@@ -11,35 +10,43 @@ from datetime import datetime
 class BaseModel:
     """
     Defines base model with common attributes and methods for other classes.
-    Attributes are id, created_at, and updated_at. It also provides methods to
-    update attributes and convert object properties to a dictionary.
+    Also supports initialization from a dictionary.
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
-        Initializes a new instance of BaseModel, setting the id, created_at,
-        and updated_at attributes.
+        Initializes a new instance of BaseModel.Dynamic initialization
+        from a dictionary through **kwargs, ignoring '__class__' key and converting
+        datetime strings back to datetime objects for 'created_at' and 'updated_at'.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                elif key in ['created_at', 'updated_at']:
+                    value = datetime.fromisoformat(value)
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
         """
-        Returns a string representation of the BaseModel instance.
+        Returns a string representation of BaseModel instance.
         """
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """
-        Updates the updated_at attribute with the current datetime.
+        Updates the 'updated_at' attribute with current datetime.
         """
         self.updated_at = datetime.now()
 
     def to_dict(self):
         """
-        Returns a dictionary containing all keys/values of __dict__ of the instance.
-        Also adds the __class__ key with the class name of the object and
-        change datetime attributes to strings in ISO format.
+        Returns a dictionary containing all keys/values of __dict__ of the instance,
+        with datetime attributes converted to strings in ISO format and adding the
+        '__class__' key with class name of the object.
         """
         dict_rep = self.__dict__.copy()
         dict_rep["__class__"] = self.__class__.__name__
